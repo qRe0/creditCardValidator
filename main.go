5613694349e77efe028684aed7e12480ce376a4b
qRe <melsenmalina@gmail.com>
2024-01-09 17:43:35 +0300

    added reading from file

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	b "bufio"
 	f "fmt"
+	io "io/ioutil"
 	"os"
 	r "regexp"
 	s "strings"
@@ -10,6 +11,8 @@ import (
 
 // Function to validate credit card number bases on the pattern
 func isCreditCardValid(ccn string) bool {
+	// Remove \n from the end of the string
+	ccn = s.TrimSpace(ccn)
 	// Regex pattern to validate credit card number (16 digits or 4 groups of 4 digits separated by space)
 	pattern := r.MustCompile(`^(\d{16})$|^(\d{4}\s\d{4}\s\d{4}\s\d{4})$`)
 
@@ -19,6 +22,8 @@ func isCreditCardValid(ccn string) bool {
 
 // Function to validate credit card number using Luhn algorithm
 func luhnAlgorithm(ccn string) bool {
+	ccn = s.TrimSpace(ccn)
+
 	sum := 0
 	alternate := false
 
@@ -40,27 +45,52 @@ func luhnAlgorithm(ccn string) bool {
 	return ans
 }
 
+func multipleCCN(fileName string) []bool {
+	content, err := io.ReadFile(fileName)
+	if err != nil {
+		f.Println("Unable to open file", fileName)
+		return nil
+	}
+
+	lines := s.Split(string(content), "\n")
+	validity := make([]bool, 0)
+
+	for _, ccn := range lines {
+		if isCreditCardValid(ccn) {
+			validity = append(validity, luhnAlgorithm(ccn))
+		} else {
+			validity = append(validity, false)
+		}
+	}
+
+	return validity
+}
+
+// Функция для печати списка валидности номеров кредитных карт
+func printValidityList(list []bool) {
+	for i, isValid := range list {
+		f.Printf("%d. %s\n", i+1, func() string {
+			if isValid {
+				return "Valid"
+			}
+			return "NOT Valid"
+		}())
+	}
+}
+
 func main() {
 	f.Println("This program will validate your credit card number")
-	f.Print("Please enter your Credit Card number: ")
-	reader := b.NewReader(os.Stdin)
-	ccn, _ := reader.ReadString('\n')
 
-	// Removing symbol '\n' from the end of the string
-	ccn = s.TrimSpace(ccn)
-
-	if !isCreditCardValid(ccn) {
-		f.Println("------------------------------------")
-		f.Println("Your credit card number is NOT valid")
-		f.Println("------------------------------------")
-		f.Println()
-		return
-	}
+	// region Maybe unused
+	var fileName string
+	var validity []bool
+	// endregion
 
 	option := -1
 	for option != 0 {
 		f.Println("Options:")
 		f.Println("1. Check if the credit card number is valid using Luhn algorithm")
+		f.Println("2. Check validity of CCN list (from file)")
 		f.Println("0. Exit")
 
 		f.Print("Please, choose an option: ")
@@ -68,6 +98,18 @@ func main() {
 
 		switch option {
 		case 1:
+			f.Print("Please enter your Credit Card number: ")
+			reader := b.NewReader(os.Stdin)
+			ccn, _ := reader.ReadString('\n')
+
+			if !isCreditCardValid(ccn) {
+				f.Println("------------------------------------")
+				f.Println("Your credit card number is NOT valid (wrong format)")
+				f.Println("------------------------------------")
+				f.Println()
+				return
+			}
+
 			if luhnAlgorithm(ccn) {
 				f.Println("-------------------------------------------------")
 				f.Println("Your credit card number is valid (Luhn algorithm)")
@@ -78,6 +120,17 @@ func main() {
 				f.Println("------------------------------------")
 			}
 			f.Println()
+			break
+		case 2:
+			f.Print("Please enter the file name: ")
+			f.Scanln(&fileName)
+
+			validity = multipleCCN(fileName)
+			f.Println("------------------------------------")
+			f.Println("CCN validity (Luhn algorithm):")
+			printValidityList(validity)
+			f.Println()
+			break
 		case 0:
 			f.Println("--------")
 			f.Println("Goodbye!")
@@ -87,6 +140,7 @@ func main() {
 			f.Println("Unknown option")
 			f.Println("--------------")
 			f.Println()
+			break
 		}
 	}
 }
