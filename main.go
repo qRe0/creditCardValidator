db4cf78551c835d588cdcc9ad0be18811f03b8b4
Yahor Malinouski <melsenmalina@gmail.com>
2024-02-29 13:30:42 +0300

    Files renamed

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("This program will validate your credit card number")
+	fmt.Println("Loading...")
+
+	var fileName string
+	var validity map[string]map[string]bool
+
+	option := -1
+	for option != 0 {
+		fmt.Println("Options:")
+		fmt.Println("1. Check if the credit card number is valid using Luhn algorithm")
+		fmt.Println("2. Check validity of CCN list (from file)")
+		fmt.Println("0. Exit")
+
+		fmt.Print("Please, choose an option: ")
+		fmt.Scanln(&option)
+
+		switch option {
+		case 1:
+			fmt.Print("Please enter your Credit Card number: ")
+			ccn, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+			if !isCreditCardValid(ccn) {
+				fmt.Println("------------------------------------")
+				fmt.Println("Your credit card number is NOT valid (wrong format)")
+				fmt.Println("------------------------------------")
+				fmt.Println()
+				break
+			}
+
+			if luhnAlgorithm(ccn) {
+				fmt.Println("-------------------------------------------------")
+				fmt.Println("Your credit card number is valid (Luhn algorithm)")
+				fmt.Println("-------------------------------------------------")
+			} else {
+				fmt.Println("------------------------------------")
+				fmt.Println("Your credit card number is NOT valid (Luhn algorithm)")
+				fmt.Println("------------------------------------")
+			}
+			fmt.Println()
+		case 2:
+			fmt.Print("Please enter the file name: ")
+			fmt.Scanln(&fileName)
+
+			validity = readCCNFromFile(fileName)
+			fmt.Println("------------------------------------")
+			fmt.Println("CCN validity (Luhn algorithm):")
+			printValidityList(validity)
+			fmt.Println()
+		case 0:
+			fmt.Println("--------")
+			fmt.Println("Goodbye!")
+			fmt.Println("--------")
+		default:
+			fmt.Println("--------------")
+			fmt.Println("Unknown option")
+			fmt.Println("--------------")
+			fmt.Println()
+		}
+	}
+}
